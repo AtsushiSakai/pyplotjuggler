1dd21f37d6b54bbbd87fbb7a09a00b014b0392e6
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-02-24 23:07:42 +0900

    keep coding

--- pyplotjuggler.py
+++ pyplotjuggler.py
@@ -12,6 +12,8 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.filedialog
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class pyplotjuggler(ttk.Frame):
@@ -34,7 +36,7 @@ class pyplotjuggler(ttk.Frame):
         """load_file"""
         ftyp = [("CSV", "*.csv")]
         idir = os.path.abspath(os.path.dirname(__file__))
-        fpath = tk.filedialog.askopenfilename(
+        fpath = tkinter.filedialog.askopenfilename(
             filetypes=ftyp, initialdir=idir)
 
         self.data = pd.read_csv(fpath)
@@ -42,15 +44,28 @@ class pyplotjuggler(ttk.Frame):
         # insert data columns
         for i, name in enumerate(self.data.columns):
             self.left.insert(i, name)
+            self.left.bind('<<ListboxSelect>>', self.on_select)
+
+    def on_select(self, evt):
+        # Note here that Tkinter passes an event object to onselect()
+        wid = evt.widget
+        index = int(wid.curselection()[0])
+        value = wid.get(index)
+        # print('You selected item %d: "%s"' % (index, value))
+
+        self.ax.plot(self.data[value])
+        self.canvas.draw()
 
     def create_widgets(self):
         """create_widgets"""
         self.languages = tk.StringVar(value=())
         self.left = tk.Listbox(self, listvariable=self.languages)
 
-        self.right = tk.Text(self)
-
         self.left.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+
+        self.fig, self.ax = plt.subplots()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        self.right = self.canvas.get_tk_widget()
         self.right.grid(column=1, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
 
         self.columnconfigure(0, weight=1)
