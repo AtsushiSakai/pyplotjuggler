a630b6f9a967b3b8c6f5da0a3b8dcc874f37e76b
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-03-10 11:14:41 +0900

    keep implementing

--- pyplotjuggler.py
+++ pyplotjuggler.py
@@ -22,8 +22,11 @@ class pyplotjuggler(ttk.Frame):
     def __init__(self, root):
         super().__init__(root)
         self.root = root
-        self.create_widgets()
+        self.figs = []
         self.data = None
+        self.selected_field = ""
+        self.create_widgets()
+        self.root.geometry('400x400')
 
         menubar = tk.Menu(root)
         filemenu = tk.Menu(menubar, tearoff=0)
@@ -51,26 +54,58 @@ class pyplotjuggler(ttk.Frame):
         wid = evt.widget
         index = int(wid.curselection()[0])
         value = wid.get(index)
-        # print('You selected item %d: "%s"' % (index, value))
-
-        self.ax.plot(self.data[value])
-        self.canvas.draw()
+        print('You selected item %d: "%s"' % (index, value))
+        self.selected_field = value
 
     def create_widgets(self):
         """create_widgets"""
         self.languages = tk.StringVar(value=())
-        self.left = tk.Listbox(self, listvariable=self.languages)
+        self.left = tk.Listbox(listvariable=self.languages)
+        self.left.pack()
 
-        self.left.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.time_slider = tk.Scale(
+            length=200, from_=0, to=200, orient=tk.HORIZONTAL)
+        self.time_slider.pack()
 
+        self.bt_new_fig = tk.Button(
+            text='Create new figure', command=self.create_new_figure)
+        self.bt_new_fig.pack()
+
+    def create_new_figure(self):
+        print("create_new_figure")
+
+        fnum = len(self.figs) + 1
+        self.figs.append(FigureManager(self, fnum))
+
+
+class FigureManager():
+    def __init__(self, parent, fnum):
         self.fig, self.ax = plt.subplots()
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
-        self.right = self.canvas.get_tk_widget()
-        self.right.grid(column=1, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.parent = parent
+        self.x = None
+        self.y = None
+        self.fig.canvas.mpl_connect('button_press_event', self.onclick)
+        self.fig_num = fnum
 
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-        self.rowconfigure(0, weight=1)
+        plt.pause(0.0001)
+
+    def onclick(self, event):
+        print('event.button=%d,  event.x=%d, event.y=%d, event.xdata=%f, \
+            event.ydata=%f' % (event.button, event.x, event.y, event.xdata, event.ydata))
+        print(self.fig_num)
+        print(self.parent.selected_field)
+        self.plot(self.parent.data[self.parent.selected_field])
+
+    def plot(self, data):
+        print("plot")
+        if self.x is None:
+            self.x = data
+            self.ax.plot(self.x)
+        elif self.y is None:
+            self.ax.cla()
+            self.y = data
+            self.ax.plot(self.x, self.y)
+        plt.pause(0.01)
 
 
 def main():
@@ -79,9 +114,6 @@ def main():
     root = tk.Tk()
     root.title('PyPlotJuggler')
     app = pyplotjuggler(root)
-    app.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
     root.mainloop()
 
     print("done!!")
