64f175cae96411c0e745f3557c6307a4cd4dab65
Atsushi Sakai <you@example.com>
2019-03-12 18:04:29 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+setup.py
+
+How to upload new release
+1. change version in setup.py
+2. python setup.py sdist upload
+
+"""
+from setuptools import setup, find_packages
+
+# read README
+try:
+    import pypandoc
+    readme = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    readme = open('README.md').read()
+
+setup(
+    name="pyplotjuggler",
+    version="0.0.1",
+    url="https://github.com/AtsushiSakai/pyplotjuggler",
+    author="Atsushi Sakai",
+    author_email="asakaig@gmail.com",
+    maintainer='Atsushi Sakai',
+    maintainer_email='asakaig@gmail.com',
+    description=("A timeseries visualization tool in Python"),
+    long_description=readme,
+    python_requires='>3.6.0',
+    license="MIT",
+    keywords="python matplotlib tkinter",
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
