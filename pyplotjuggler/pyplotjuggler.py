0396d554eda1024b8e1045ef70e50ca5a9e4a718
Atsushi Sakai <you@example.com>
2019-03-13 17:04:23 +0900

    add time seek function

--- pyplotjuggler/pyplotjuggler.py
+++ pyplotjuggler/pyplotjuggler.py
@@ -29,20 +29,24 @@ class pyplotjuggler(ttk.Frame):
         self.figs = []
         self.data = None
         self.selected_field = ""
+        self.time = 0
         self.create_widgets()
         self.root.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT))
 
-        # Menu bar setting
-        menubar = tk.Menu(root)
-        filemenu = tk.Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Load", command=self.load_file)
-        menubar.add_cascade(label="File", menu=filemenu)
-        helpmenu = tk.Menu(menubar, tearoff=0)
-        helpmenu.add_command(label="About", command=self.show_about_message)
-        menubar.add_cascade(label="Help", menu=helpmenu)
+        self.setup_menubar()
 
-        root.config(menu=menubar)
         root.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def setup_menubar(self):
+        self.menubar = tk.Menu(self.root)
+        filemenu = tk.Menu(self.menubar, tearoff=0)
+        filemenu.add_command(label="Load", command=self.load_file)
+        self.menubar.add_cascade(label="File", menu=filemenu)
+        helpmenu = tk.Menu(self.menubar, tearoff=0)
+        helpmenu.add_command(label="About", command=self.show_about_message)
+        self.menubar.add_cascade(label="Help", menu=helpmenu)
+
+        self.root.config(menu=self.menubar)
 
     def on_closing(self):
         """ on closing widget"""
@@ -80,12 +84,19 @@ class pyplotjuggler(ttk.Frame):
         messagebox.showinfo("About", msg)
 
     def on_select(self, evt):
+        """on_select"""
         # Note here that Tkinter passes an event object to onselect()
         wid = evt.widget
         index = int(wid.curselection()[0])
         value = wid.get(index)
-        print('You selected item %d: "%s"' % (index, value))
+        # print('You selected item %d: "%s"' % (index, value))
         self.selected_field = value
+
+    def slider_changed(self, event):
+        self.time = self.time_slider.get()  # update time
+
+        for f in self.figs:
+            f.plot_time_line(self.time)
 
     def create_widgets(self):
         """create_widgets"""
@@ -95,11 +106,21 @@ class pyplotjuggler(ttk.Frame):
 
         self.time_slider = tk.Scale(
             length=WINDOW_WIDTH, from_=0, to=200, orient=tk.HORIZONTAL)
+        self.time_slider.bind("<ButtonRelease-1>", self.slider_changed)
         self.time_slider.pack()
 
         self.bt_new_fig = tk.Button(
             text='Create new figure', command=self.create_new_figure)
         self.bt_new_fig.pack()
+
+        self.setup_status_bar()
+
+    def setup_status_bar(self):
+        self.status_bar_str = tkinter.StringVar()
+        self.status_bar = tkinter.Label(self.root, textvariable=self.status_bar_str,
+                                        bd=1, relief=tkinter.SUNKEN, anchor=tkinter.W)
+        self.status_bar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
+        self.status_bar_str.set("")
 
     def create_new_figure(self):
         print("create_new_figure")
@@ -124,17 +145,30 @@ class FigureManager():
             event.ydata=%f' % (event.button, event.x, event.y, event.xdata, event.ydata))
         print(self.fig_num)
         print(self.parent.selected_field)
-        self.plot(self.parent.data[self.parent.selected_field])
+        self.set_data(self.parent.data[self.parent.selected_field])
 
-    def plot(self, data):
+    def set_data(self, data):
+
         if self.x is None:
             self.x = data
-            self.ax.plot(self.x)
         elif self.y is None:
-            self.ax.cla()
             self.y = data
-            self.ax.plot(self.x, self.y)
+
+        self.plot()
+
+    def plot(self):
+
+        if self.y is None:
+            time = [t for t in range(len(self.x))]
+            self.ax.plot(time, self.x)
+
+        self.ax.grid(True)
         plt.pause(0.01)
+
+    def plot_time_line(self, time):
+        self.ax.cla()
+        self.ax.axvline(x=time)
+        self.plot()
 
 
 def main():
