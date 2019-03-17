3e7f6a3b06322206ab4d9d1af8622756e8a6334d
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-03-17 14:40:51 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except(IOError, ImportError):
 
 setup(
     name="pyplotjuggler",
-    version="0.0.1",
+    version="0.1.0",
     url="https://github.com/AtsushiSakai/pyplotjuggler",
     author="Atsushi Sakai",
     author_email="asakaig@gmail.com",
